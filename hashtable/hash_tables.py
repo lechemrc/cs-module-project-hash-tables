4625e1a3a5ce2edf36f4b9892a1c0ddbdacb7e5d
Rob LeCheminant <roblecheminant@gmail.com>
2020-09-01 19:48:04 -0600

    adding lecture note file

--- hashtable/hash_tables.py
+++ hashtable/hash_tables.py
@@ -15,3 +15,11 @@ def find_element(arr, el):
 
 # Which big ) complexities are faster than log n?
 ## Constant time: O(1)
+### If we increase the input, we still take number of steps to find what we're looking for
+
+def magic_func_find_index(arr, el):
+    return el_index
+
+idx = magic_func_find_index(my_arr, 'set') # 7
+my_arr[idx] # ta-da
+
