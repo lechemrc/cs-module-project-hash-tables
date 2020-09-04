6e0898ce54dd499c29ce0a0087873a6d8e9efc39
Rob LeCheminant <roblecheminant@gmail.com>
2020-09-03 21:56:21 -0600

    updating lecture notes

---
+++ hashtable/hash_tables_2.py
@@ -0,0 +1,152 @@
+## Review project
+## Code our hash functions
+## Collisions: how to handle
+## Load Factor
+## Stretch goal: hash function applications
+
+## In general, crypto is hard
+### takes any string as input
+### Deterministic
+### Non-reversible
+
+### May have to generate a "secret" that the function uses to hash/encrypt
+
+# COLLISIONS
+## How should we handle?
+## Disallow it?
+## Open addressing: linear probing, quadratic probing
+
+## Linked list aka chaining
+## We're gonna make an array of linked lists
+## data structure composition
+
+
+
+# -----------------------------------------------
+# buckets = [None] * 8
+
+# buckets = [
+#     0  None, 
+#     1  Node('foo', 'bar') --> Node('foooz', 'fub') --> Node('zug', 'ixq')
+#     2  None, 
+#     3  Node('quux', 'baz'), 
+#     4  None, 
+#     5  None, 
+#     6  None, 
+#     7  None, 
+#     ]
+
+# put('foo', 'bar') # 'foo' hashes to index 1
+# put('quux', 'baz') # index 3
+# put('foooz', 'fub') # 'foooz' hashes to index 1
+# put('zug', 'ixq') # 'zug' hashes to index 1
+
+# get('foooz') # go to index 1, iterate through and find the same key, return value 'fub'
+# ​
+# put('zug', 'ziggy') # go to index 1, iterate through and find the same key, overwrite
+# ​
+# my_dict['a'] = 1
+# my_dict['a'] = 2
+# -----------------------------------------------
+
+
+
+## time cost of iterating? O(n)?????
+## how does the linked list and array work together in memory?
+
+## storing key is a bad idea if you're hashing passwords
+## but for a hash table it's okay
+
+## Put
+### hash your key, mod by len of your array to get your idx
+### If idx is None, put your Node there
+### If not None, iterate through
+#### If same key is found, overwrite
+#### If not, append to head or tail
+
+## Get
+### hash your key, mod by len of your array to get your idx
+### If idx is None, return None
+### If idx isn't None, iterate through and find matching key and return the value
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    def find(self, val):
+        # assign a cur_node
+        cur_node = self.head
+        # while cur_node is not Noe
+        while cur_node is not None:
+            # check the value
+            # if self.value = val
+            if val == cur_node.value:
+            # return self.val
+                return cur_node.value
+            # else cur_node = cur_node.next
+            else: 
+                cur_node = cur_node.next
+
+        return None
+
+    def add_to_tail(self, val):
+        """
+        Check if we have a head
+        If so, find the tail and add a node
+        """
+
+        # if head is None, make a node and add it
+        if self.head is None:
+            self.head = Node(val)
+        else:
+        # otherwise, iterate through and find the tail
+            cur_node = self.head
+            while cur_node.next is not None:
+                cur_node = cur_node.next
+            
+            cur_node.next = Node(val)
+
+
+ll = LinkedList()
+n1 = Node(3)
+ll.head = n1
+
+ll.add_to_tail(4)
+ll.add_to_tail(5)
+ll.add_to_tail(6)
+
+
+## Load Factor
+# O(1)
+"""
+0 |-> D
+1 |-> H -> I -> J
+2 |-> A
+3 |-> C -> K -> L -> O -> P
+4 |-> G -> Q -> R -> S
+5 |-> B -> M
+6 |-> E -> N
+7 |-> F
+​"""
+
+# 19/8 == 2.375
+## number of elements / number of buckets
+
+## load factor < 0.7
+## if load factor is < 0.2, using unnecessary amounts of memory
+
+# Resize
+## Double the backing array!
+## Then copy everything over
+### Go through every bucket (aka index in your array)
+### Go through every node in the linked list (which lives in that bucket)
+
+## That's O(n) but we don't resize very often
+
+## hash it and mod it by the size of your array
--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -16,14 +16,17 @@ class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
     def __init__(self, capacity):
-        self.capacity = capacity
-        self.storage = [None] * capacity
-        self.count = 0
+        if capacity < MIN_CAPACITY:
+            self.capacity = MIN_CAPACITY
+        else:
+            self.capacity = capacity
+
+        self.storage = [None] * self.capacity
+        self.size = 0
 
 
     def get_num_slots(self):
@@ -31,9 +34,7 @@ class HashTable:
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
         One of the tests relies on this.
-
         Implement this.
         """
         return self.capacity
@@ -42,7 +43,6 @@ class HashTable:
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
         Implement this.
         """
         return self.count / self.capacity
@@ -51,7 +51,6 @@ class HashTable:
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
-
         Implement this, and/or DJB2.
         """
 
@@ -59,25 +58,26 @@ class HashTable:
         offset_basis = 14695981039346656037
 
         #FNV-1a Hash Function
-        hash_val = offset_basis + key
-        key_utf8 = key.encode()
+        hash_val = offset_basis
+        key_bytes = key.encode()
 
-        for char in key_utf8:
+        for byte in key_bytes:
             hash_val = hash_val * FNV_prime
-            hash_val = hash_val ^ ord(char)
+            hash_val = hash_val ^ byte
+
         return hash_val
 
 
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
         Implement this, and/or FNV-1.
         """
-        hash = 5381
-        for x in s:
-            hash = (( hash << 5) + hash) + ord(x)
-        return hash & 0xFFFFFFFF
+        hash_val = 5381
+        key_bytes = key.encode()
+        for byte in key_bytes:
+            hash_val = (( hash_val << 5) + hash_val) + byte
+        return (hash_val & 0xFFFFFFFF) # 0xFFFFFFFF means: take only the right-most 8 digits
 
 
     def hash_index(self, key):
@@ -91,41 +91,50 @@ class HashTable:
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
-        # Your code here
+        # 1. Hash the key
+        # 2. Take the hash and mod it with len of array
+        idx = self.hash_index(key)
+        # 3a. Check if there is a collision
+        if self.storage[idx] != None:
+            print('WARNING! You are overwriting a value!')
+        # 3b. Go to index and put in value
+        self.storage[idx] = value
 
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
-        # Your code here
+        # Find index for given key
+        idx = self.hash_index(key)
+        # Set index to None
+        self.storage[idx] = None
 
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
-        # Your code here
+        # 1. Hash the key
+        # 2. Take the hash and mod it with len of array
+        idx = self.hash_index(key)
+        # 3. Go to index and get out value
+        value = self.storage[idx]
+
+        return value
 
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
         Implement this.
         """
         # Your code here
