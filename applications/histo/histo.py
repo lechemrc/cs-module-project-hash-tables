d6d7bed5c6d6aae1a48a0b95525a6c1209e16db9
Rob LeCheminant <roblecheminant@gmail.com>
2020-09-14 19:12:02 -0600

    updating a couple files

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,19 @@
-# Your code here
+def histo(s):
+    cache = {}
+    drop_chars = [
+        '\"', '\'', ':', ';', ',', '.', '-', '+', '=', '/', 
+        '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&'
+        ]
+
+    no_punc = s.lower().split()
+    for chars in drop_chars:
+        no_punc = [i.replace(chars, '') for i in no_punc]
+
+    for word in no_punc: 
+        if word in cache: 
+            cache[word] += 1
+        else: 
+            cache[word] = 1
+
+    return cache
 
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,4 +1,4 @@
-import string
+# import string
 
 def word_count(s):
     cache = {}
@@ -12,7 +12,7 @@ def word_count(s):
         no_punc = [i.replace(chars, '') for i in no_punc]
 
     for word in no_punc: 
-        if (word in cache): 
+        if word in cache: 
             cache[word] += 1
         else: 
             cache[word] = 1
